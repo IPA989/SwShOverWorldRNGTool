bdaf002afdb7914eb1838fdd134a39586b690685
uni <77717272+IPA989@users.noreply.github.com>
2022-02-06 13:14:05 +0900

    Add version and privacy policy

--- SwShOverWorldRNGTool/app/src/main/java/com/ipa989/swshoverworldrngtool/Settings.java
+++ SwShOverWorldRNGTool/app/src/main/java/com/ipa989/swshoverworldrngtool/Settings.java
@@ -2,7 +2,11 @@ package com.ipa989.swshoverworldrngtool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Settings extends AppCompatActivity {
 
@@ -10,6 +14,18 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        String name = "com.ipa989.swshoverworldrngtool";
+        Context context = getApplicationContext();
+        PackageManager pm = context.getPackageManager();
+        PackageInfo info = null;
+        try {
+            info = pm.getPackageInfo(name, PackageManager.GET_META_DATA);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        TextView ve = findViewById(R.id.version);
+        ve.setText(info.versionName);
     }
 
 }
